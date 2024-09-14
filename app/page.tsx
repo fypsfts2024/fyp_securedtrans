9b5499f034096fb1ca696dc2f703b7a33921859c
pheymin <pheymin1223@gmail.com>
2024-09-14 16:01:20 +0800

    feat:init

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,12 @@
-import Hero from "@/components/hero";
-import ConnectSupabaseSteps from "@/components/tutorial/connect-supabase-steps";
-import SignUpUserSteps from "@/components/tutorial/sign-up-user-steps";
-import { hasEnvVars } from "@/utils/supabase/check-env-vars";
-
 export default async function Index() {
-  return (
-    <>
-      <Hero />
-      <main className="flex-1 flex flex-col gap-6 px-4">
-        <h2 className="font-medium text-xl mb-4">Next steps</h2>
-        {hasEnvVars ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
-      </main>
-    </>
-  );
+    return (
+        <>
+            <div className="flex flex-col gap-16 items-center">
+                <p className="text-3xl lg:text-4xl !leading-tight mx-auto max-w-xl text-center">
+                    Your files, your control.
+                </p>
+                <div className="w-full p-[1px] bg-gradient-to-r from-transparent via-foreground/10 to-transparent my-8" />
+            </div>
+        </>
+    );
 }
